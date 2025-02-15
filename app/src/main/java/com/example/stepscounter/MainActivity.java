f92612742b05201c1ed0d90954e524f37b7c0d34
wael <waelferjeni86@gmail.com>
2025-02-15 07:49:34 +0100

    c

---
+++ app/src/main/java/com/example/stepscounter/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.stepscounter;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager = null;
+    private Sensor stepSensor;
+    private int totalSteps = 0;
+    private int previewsTotalSteps = 0;
+    private ProgressBar progressBar;
+    private TextView steps;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        steps = findViewById(R.id.steps);
+        resetSteps();
+        loadData();
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        stepSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+    }
+
+    protected void  onResume(){
+        super.onResume();
+
+        if (stepSensor == null){
+            Toast.makeText(this, "This device has no sensor", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            mSensorManager.registerListener(this,stepSensor,SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    protected  void onPause(){
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_STEP_COUNTER){
+            totalSteps = (int) event.values[0];
+            int currentSteps = totalSteps-previewsTotalSteps;
+            steps.setText(String.valueOf(currentSteps));
+
+            progressBar.setProgress(currentSteps);
+        }
+
+    }
+
+    private  void resetSteps(){
+        steps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Long press to reset steps", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        steps.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                previewsTotalSteps = totalSteps;
+                steps.setText("0");
+                progressBar.setProgress(0);
+                savedData();
+                return false;
+            }
+        });
+    }
+
+    private void savedData(){
+        SharedPreferences sharedPref = getSharedPreferences("myPref", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("key1",String.valueOf(previewsTotalSteps));
+        editor.apply();
+    }
+
+    private void loadData(){
+        SharedPreferences sharedPref = getSharedPreferences("myPref", Context.MODE_PRIVATE);
+        int savedNumber = (int) sharedPref.getFloat("key1", 0f);
+        previewsTotalSteps = savedNumber;
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
